292aab4ab5f4332ad08c423079c2a2fa0bc22c3a
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2025-11-03 10:18:56 -0300

    Classe contandoNumeros criada

---
+++ src/contandoNumeros.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class contandoNumeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        for(int i = 1; i <= numero; i++){
+            System.out.print(i);
+        }
+    }
+}
